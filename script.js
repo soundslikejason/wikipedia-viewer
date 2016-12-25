9ce0df7206453cacd4060c4905085db02c23744e
Jason Leong <soundslikejason@users.noreply.github.com>
2016-12-25 14:10:40 -0800

    api url and ajax

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $('#btn').click(function() {
-    var term = $('#term').val();
+    var searchTerms = $('#term').val();
+    var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+term+"&format=json&callback=?";
+
+    $.ajax({
+      type: 'GET',
+      url: apiURL,
+      dataType: 'json',
+      success: function(data) {}
+    });
   });
-}); 
+});
