849ac72d32dfe4b5a739b94626767e3ac7218587
Jason Leong <soundslikejason@users.noreply.github.com>
2016-12-25 15:54:34 -0800

    output results to html

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
-  $('#btn').click(function() {
-    var searchTerms = $('#term').val();
-    var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+term+"&format=json&callback=?";
+  $('#searchButton').click(function() {
+    var searchTerms = $('#searchTerms').val();
+    var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerms + "&format=json&callback=?";
 
     $.ajax({
       type: 'GET',
       url: apiURL,
       dataType: 'json',
-      success: function(data) {}
+      success: function(data) {
+        $('#results').html('');
+        for (i = 0; i < data[1].length; i++) {
+          $('#results').append("<li><a href=" + data[3][i] + "target='_blank'>" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
+        }
+      }
     });
   });
 });
